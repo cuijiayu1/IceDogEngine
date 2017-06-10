a396dd3e078bc6792191da7d3339568780ac385d
yuletian <469632477@qq.com>
2017-06-10 14:17:24 +0800

    correct tone mapping

--- IceDogEngine/Shaders/DeferredIBLPS.hlsl
+++ IceDogEngine/Shaders/DeferredIBLPS.hlsl
@@ -166,7 +166,7 @@ float4 CosineSampleHemisphere(float2 E)
 
 float4 cubeMapInst(float4 color)
 {
-	return color*8;
+	return color;
 }
 
 float3 PrefilterEnvMap(float Roughness, float3 R) {
--- IceDogEngine/Shaders/FusePS.hlsl
+++ IceDogEngine/Shaders/FusePS.hlsl
@@ -31,7 +31,7 @@ LightPSOut main(LightGSOut vout)
 	LightPSOut result;
 	//result.finalColor = lightColorOut.Sample(samAnisotropic, vout.uv);
 	//result.finalColor = ACESToneMapping(hehe.Sample(samAnisotropic, vout.uv)*6, 0.5);
-	result.finalColor = ACESToneMapping(lightColorOut.Sample(samAnisotropic, vout.uv), 0.5);
+	result.finalColor = ACESToneMapping(lightColorOut.Sample(samAnisotropic, vout.uv), 4);
 	/*if (result.finalColor.r > 3 || result.finalColor.g > 3 || result.finalColor.b > 3)
 	{
 		result.finalColor = float4(0, 1, 0, 1);
